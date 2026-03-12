0a024f6ce60360fdb9dbd0d43f4620760ca6fb7a
Mariela Mejía Gutiérrez <98550508+marielamejia@users.noreply.github.com>
2026-03-12 17:57:32 -0600

    .

---
+++ HTMLSeguros/script.js
@@ -0,0 +1,9 @@
+//aqui ponemos la funcionalidad de nuestro formulario de seguros
+
+//variables para guardar valores del forms
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido"); 
+const edad = document.getElementById("edad"); 
+const estado = document.getElementById("estado"); 
+
+
